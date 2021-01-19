e77c2971d5e17e3bc1c49ccbeec3cf21509b7355
PRAVEEN J <praveen.j@microfocus.com>
2021-01-19 22:26:08 +0530

    PathSum problem for Binary Tree

---
+++ Tree/PathSumForBinaryTree.java
@@ -0,0 +1,36 @@
+package Tree;
+
+public class PathSumForBinaryTree {
+
+    /**
+     *
+     *
+     * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+     * A leaf is a node with no children.
+     *
+     * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+     * Output: true
+     *
+     */
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root==null)
+            return false;
+        return dfs(root,targetSum);
+    }
+
+    private boolean dfs(TreeNode root,int targetSum){
+        if(root==null)
+            return false;
+        if(targetSum==root.val && root.left==null && root.right==null)
+            return true;
+        return dfs(root.left,targetSum-root.val) || dfs(root.right,targetSum-root.val);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
