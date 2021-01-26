d0881ddb209030fcea0767b989f3a64b2f1a901a
PRAVEEN J <praveen.j@microfocus.com>
2021-01-26 22:49:48 +0530

    Merge Two Binary Trees

---
+++ Tree/MergeTwoBinaryTrees.java
@@ -0,0 +1,83 @@
+package Tree;
+
+import java.util.Stack;
+
+public class MergeTwoBinaryTrees {
+    /**
+     * Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+     *
+     * You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+     *
+     * Example 1:
+     *
+     * Input:
+     * 	Tree 1                     Tree 2
+     *           1                         2
+     *          / \                       / \
+     *         3   2                     1   3
+     *        /                           \   \
+     *       5                             4   7
+     * Output:
+     * Merged tree:
+     * 	     3
+     * 	    / \
+     * 	   4   5
+     * 	  / \   \
+     * 	 5   4   7
+     *
+     *
+     * Note: The merging process must start from the root nodes of both trees.
+     *
+     */
+
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if(t1==null)
+            return t2;
+
+        if(t2==null)
+            return t1;
+        //solution using recursion
+        t1.val+=t2.val;
+        t1.left=mergeTrees(t1.left,t2.left);
+        t1.right=mergeTrees(t1.right,t2.right);
+        return t1;
+
+        //solution using iterative approach using stack
+
+      /*  Stack<TreeNode[]> stack=new Stack();
+
+        stack.push(new TreeNode[] {t1,t2});
+
+
+
+        while(!stack.isEmpty()){
+
+
+            TreeNode[] t=stack.pop();
+
+            if(t[0]==null || t[1]==null )
+                continue;
+
+            t[0].val+=t[1].val;
+
+            if(t[0].left==null){
+                t[0].left=t[1].left;
+            }
+            else{
+                stack.push(new TreeNode[]{t[0].left,t[1].left});
+            }
+            //check for the right nodes
+            if(t[0].right==null){
+                t[0].right=t[1].right;
+            }
+            else{
+                stack.push(new TreeNode[]{t[0].right,t[1].right});
+            }
+        }
+
+
+        return t1;*/
+    }
+}
